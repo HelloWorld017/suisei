2e566cf408178d39e485df73ff374f65f5d28df8
Khinenw <deu07115@gmail.com>
2022-06-19 15:54:37 +0900

    feat(reactivity): implement decompose primitive

---
+++ packages/reactivity/src/primitives/decompose.ts
@@ -0,0 +1,48 @@
+import { readRef } from '../utils';
+import { throwError } from '@suisei/shared';
+import { ErrorCodes } from '@suisei/shared';
+import type { Ref } from '../types';
+
+type Decomposed<T> = 
+	T extends object
+		? { [K in keyof T]: Decomposed<T[K]> } & Ref<T>
+		: Ref<T>;
+
+export const decompose = <T>(ref: Ref<T>): Decomposed<T> =>
+	new Proxy<Decomposed<T>>(
+		ref as Decomposed<T>,
+		{
+			get(target, key) {
+				if (Object.prototype.hasOwnProperty.call(target, key)) {
+					return target[key as keyof Decomposed<T>];
+				}
+				
+				return decompose(_ => {
+					const value = _(target);
+					if (!(key in value)) {
+						return undefined;
+					}
+					
+					return (value as T)[key as keyof T];
+				});
+			},
+			
+			set(_target, key) {
+				return throwError(ErrorCodes.E_SET_ON_DECOMPOSE, key);
+			},
+			
+			ownKeys(target) {
+				const value = readRef(target);
+				return Reflect.ownKeys(value).concat(Reflect.ownKeys(target));
+			},
+			
+			has(target, key) {
+				const value = readRef(target);
+				return Reflect.has(value, key) || Reflect.has(target, key);
+			},
+			
+			isExtensible() {
+				return false;
+			}
+		}
+	);
--- packages/shared/src/utils/errors.ts
+++ packages/shared/src/utils/errors.ts
@@ -2,11 +2,13 @@ import { SymbolIs, SymbolSuspendDerivation } from '@suisei/shared';
 export const enum ErrorCodes {
 	E_NOT_REFERENCE,
 	E_NOT_PROMISE,
+	E_SET_ON_DECOMPOSE,
 };
 
 export const ErrorMessages = {
 	[ErrorCodes.E_NOT_REFERENCE]: 'Given value $1 is not a ref.',
 	[ErrorCodes.E_NOT_PROMISE]: 'Given value $1 is not a promise.',
+	[ErrorCodes.E_SET_ON_DECOMPOSE]: 'Tried to set key $1 to a decomposed ref',
 } as const;
 
 export const throwError = (code: ErrorCodes, ...args: any[]): never => {
