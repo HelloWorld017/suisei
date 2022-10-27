8878ea82bf7bfea159714c9c9ba6acbd5c979672
Khinenw <deu07115@gmail.com>
2022-10-28 06:39:03 +0900

    chore(examples): update package config

--- examples/helloworld/index.tsx
+++ examples/helloworld/index.tsx
@@ -1,6 +1,8 @@
-import { renderToStream } from 'suisei/dom-server';
+import { renderToStream } from 'suisei-dom/server';
 import http from 'node:http';
 
-http.createServer(async (req, res) => {
+const server = http.createServer(async (req, res) => {
   await renderToStream(res, <div>Hello, World! from {req.url as string}</div>);
 });
+
+server.listen(3000);
