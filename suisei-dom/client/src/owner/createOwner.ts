77f523bfb48c2fb0a1a872c9f0929bc25c015bbe
nenw <khi@nenw.dev>
2023-03-12 18:47:09 +0900

    feat(client): implement client owner

--- suisei-dom/client/src/owner/createOwner.ts
+++ suisei-dom/client/src/owner/createOwner.ts
@@ -1,5 +1,5 @@
 import { Ref } from 'suisei';
-import type {
+import {
   EffectCleanup,
   EffectRunAt,
   Owner,
@@ -7,91 +7,126 @@ import type {
   Task,
 } from 'suisei/unsafe-internals';
 
-export const createOwner = (scheduler: Scheduler): Owner => {
-  let pendingUpdates = new Map<Ref, Task>();
-  let pendingEffects: Task[] = [];
-  let pendingLayoutEffects: Task[] = [];
+type TaskLayer<K = unknown> = {
+  commit(callback?: () => void): void;
+  scheduleTask(task: Task, key?: K): void;
+  hasTask(): boolean;
+};
 
-  const addEffectTask = (runAt: EffectRunAt, effectTask: Task) => {
-    if (runAt === 'layout') {
-      pendingLayoutEffects.push(effectTask);
-      return;
-    }
+const createTaskLayer = <K = unknown>(
+  scheduler: Scheduler,
+  previousLayer?: TaskLayer<unknown>
+): TaskLayer<K> => {
+  let maxTaskId = 0;
+  let pendingUpdates = new Map<K | number, Task>();
 
-    pendingEffects.push(effectTask);
+  const scheduleTask = (task: Task, key?: K) => {
+    pendingUpdates.set(key ?? maxTaskId, task);
+    maxTaskId += 1;
   };
 
-  const assignUpdatesToScheduler = (callback: () => void) => {
+  const hasTask = () => pendingUpdates.size > 0;
+
+  const commit = (callback: () => void) => {
     const currentlyAssignedTasks = pendingUpdates;
     pendingUpdates = new Map();
+    maxTaskId = 0;
 
     currentlyAssignedTasks.forEach(task => scheduler.queueTask(task));
     scheduler.queueTask(() => {
-      if (pendingUpdates.size > 0) {
-        return assignUpdatesToScheduler(callback);
+      if (previousLayer?.hasTask()) {
+        previousLayer.commit(() => commit(callback));
+        return;
       }
 
-      callback();
+      if (hasTask()) {
+        commit(callback);
+        return;
+      }
+
+      callback?.();
     });
   };
 
-  const assignLayoutEffectsToScheduler = (callback: () => void) => {
-    const currentlyAssignedLayoutEffects = pendingLayoutEffects;
-    pendingLayoutEffects = [];
+  return { commit, scheduleTask, hasTask };
+};
 
-    currentlyAssignedLayoutEffects.forEach(task => scheduler.queueTask(task));
-    assignUpdatesToScheduler(() =>
-      scheduler.queueTask(() => {
-        if (pendingLayoutEffects.length > 0) {
-          return assignLayoutEffectsToScheduler(callback);
-        }
+const createUpdater = (scheduler: Scheduler, update: () => void) => {
+  let isDirty = false;
+  const markAsDirty = () => {
+    if (isDirty) {
+      return;
+    }
 
-        callback();
-      })
-    );
+    isDirty = true;
+    scheduler.queueTask(() => {
+      isDirty = false;
+      update();
+    });
   };
 
-  const assignEffectsToScheduler = (callback: () => void) => {
-    const currentlyAssignedEffects = pendingEffects;
-    pendingEffects = [];
+  return markAsDirty;
+};
 
-    currentlyAssignedEffects.forEach(task => scheduler.queueTask(task));
-    assignLayoutEffectsToScheduler(() =>
-      scheduler.queueTask(() => {
-        if (pendingEffects.length > 0) {
-          return assignEffectsToScheduler(callback);
-        }
+export const createOwner = (
+  scheduler: Scheduler,
+  onSuspense: (promise: Promise<unknown>) => void,
+  onError: (error: unknown) => void
+): Owner => {
+  const updateLayer = createTaskLayer<Ref>(scheduler);
+  const layoutEffectLayer = createTaskLayer(scheduler, updateLayer);
+  const defaultEffectLayer = createTaskLayer(scheduler, layoutEffectLayer);
 
-        callback();
-      })
-    );
+  const effectLayers: Record<EffectRunAt, TaskLayer> = {
+    layout: layoutEffectLayer,
+    default: defaultEffectLayer,
   };
+
+  const markAsDirty = createUpdater(scheduler, () => {
+    updateLayer.commit(() => {
+      layoutEffectLayer.commit(() => {
+        defaultEffectLayer.commit();
+      });
+    });
+  });
 
   const cleanups: EffectCleanup[] = [];
 
   return {
     stateCount: 0,
     onStateUpdate(ref, _flags, runUpdate) {
-      pendingUpdates.set(ref, runUpdate);
+      updateLayer.scheduleTask(runUpdate, ref);
+      markAsDirty();
     },
+
     onDeriveUpdateByObserve(ref, _flags, runDerive) {
-      pendingUpdates.set(ref, runDerive);
+      updateLayer.scheduleTask(runDerive, ref);
+      markAsDirty();
     },
+
     onEffectInitialize(runAt, runEffect) {
-      addEffectTask(runAt, () => {
+      effectLayers[runAt].scheduleTask(() => {
         cleanups.push(runEffect());
       });
+      markAsDirty();
     },
-    onEffectUpdate: addEffectTask,
+
+    onEffectUpdate(runAt, runUpdate) {
+      effectLayers[runAt].scheduleTask(runUpdate);
+      markAsDirty();
+    },
+
     onFutureInitialize(promise, cleanup) {
-      // TODO
+      onSuspense(promise);
       cleanups.push(cleanup);
     },
+
     onFutureUpdate(promise, _flags) {
-      // TODO
+      onSuspense(promise);
     },
+
     onError(error) {
-      // TODO
+      onError(error);
     },
   };
 };
